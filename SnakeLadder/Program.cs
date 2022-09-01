a4e111e263cfc57aa6dcafa153fda55ad1578d47
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-09-01 15:14:22 +0530

    [Anjali]For Single Player

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -7,6 +7,71 @@
             Console.WriteLine("Snake and Ladder Game with single player");
             int position = 0;
             Console.WriteLine("Starting position is : "+position);
+            int diecount = 1;
+
+            while (position < 10)
+            {
+
+                Console.WriteLine("Roll a Die :");
+                Random random = new Random();
+                int r = random.Next(1, 7);
+                Console.WriteLine("Number is " + r);
+
+
+                //getting the position of player
+                if ((position += r) >= 10)
+                {
+                    position = position - r;
+                    Console.WriteLine("Position is " + position);
+                }
+                else
+                {
+                    position = position;
+                }
+                    Random random1 = new Random();
+                    int a = random1.Next(0, 3);
+                    Console.WriteLine("Checking for options No play,Ladder or Snake ");
+
+                    switch (a)
+                    {
+                        case 0:
+                            Console.WriteLine("No play ");
+                            //position =  ;
+                            Console.WriteLine("Position is same " + position);
+                            break;
+                        case 1:
+                            if ((position + r) > 10)
+                                break;
+                            else
+                                Console.WriteLine("There is a Ladder");
+                            position += r;
+                            Console.WriteLine("Incremented position is " + position);
+
+                            break;
+                        case 2:
+                            Console.WriteLine("There is a Snake");
+                        if (position > 0)
+                        {
+                            position -= r;
+                            Console.WriteLine("Decremented position is " + position);
+                        }
+                        else
+                            position = 0;
+                            break;
+                    }
+                
+                    diecount++;
+                
+            }
+            Console.WriteLine("Total number of Dice played are " +diecount);
+
+
+                
+                
+
+                
+
+
 
         }
     }
---
+++ SnakeLadder/RollDie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class RollDie
+    {
+        Random random = new Random();
+       // int a=Convert.ToInt32(random.Next(1,7));
+
+
+    }
+}
