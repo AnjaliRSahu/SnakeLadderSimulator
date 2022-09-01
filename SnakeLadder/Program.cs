04e9299be52f0b50a656345d927d4eeec46b07a7
AnjaliRSahu <anjalisahu1996@gmail.com>
2022-09-01 12:53:39 +0530

    [Anjali] Game Started

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder Game with single player");
+            int position = 0;
+            Console.WriteLine("Starting position is : "+position);
+
+        }
+    }
+}
